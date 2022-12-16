5c0f68e5d391d26f945111e4e4cccfd08e7885a7
nigel-rgb <nigel@ionmobility.com>
2022-12-16 15:32:52 +0800

    modified to copy whole folder

--- copy_paste.ps1
+++ copy_paste.ps1
@@ -1,23 +1,37 @@
-$copy_file  = '.\Gridania-Realtime\src\*'
-$paste_file = '.\Gridania_RealTime118\System\StateManagement\'
+$copy_c  = '..\Gridania-Realtime\src\src'
+$paste_c = '..\Gridania_RealTime118\System\StateManagement\src'
+$copy_h  = '..\Gridania-Realtime\src\inc'
+$paste_h = '..\Gridania_RealTime118\System\StateManagement\inc'
 
 
 # function print {
 
 #     process {}
-# }
 
 
-if ($copy_file -AND $paste_file) {
+
+if ($copy_c -AND $paste_c) {
 
     Write-Host -Object 'Source: ' -NoNewline
-    Write-Output $copy_file
+    Write-Output $copy_c
     Write-Host -Object 'Destination: ' -NoNewline
-    Write-Output $paste_file
+    Write-Output $paste_c
     $confirmation = Read-Host  'Continue with path? [Y]/[N]'
 
+
     if ($confirmation -eq 'y') {
-        replace $copy_file $paste_file /s
+        Remove-Item $paste_c -Recurse
+        Write-Host -Object 'Removed' -NoNewline
+        Write-Output $paste_c
+        
+        Remove-Item $paste_h -Recurse
+        Write-Host -Object 'Removed' -NoNewline
+        Write-Output $paste_h
+        
+        Copy-Item -Path $copy_c -Destination $paste_c -Recurse
+        Copy-Item -Path $copy_h -Destination $paste_h -Recurse
+        #replace $copy_c $paste_c /s
+        #replace $copy_h $paste_h /s
     }
 
 
