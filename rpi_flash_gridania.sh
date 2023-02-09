62e7da3487e8b005070681024e3372dfcf998eb7
nigel-rgb <nigel@ionmobility.com>
2023-02-09 17:47:42 +0800

    added comments

--- rpi_flash_gridania.sh
+++ rpi_flash_gridania.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# add 118 srec directory
+dir_118="/a/gitclone/Gridania_RealTime118/GCC/release/gridania-realtime.srec"
+dir_148="/a/gitclone/Gridania_Telematic148/GCC/release/gridania-telematic.srec"
+
+# add rpi address
+rpi_add="pi@raspberrypi-"
+
 
 # Parse command line options
 while getopts "r:e:" opt; do
@@ -21,29 +28,25 @@ done
 # Check if the host option was specified
 if [ -z "$rpi_version" ]; then
   echo "Error: Enter RPI number with argument -r (either 7 or 75)" >&2
-  exit 
+  exit 1
 fi
 
 # Check if the username option was specified
 if [ -z "$ecu" ]; then
   echo "Error: Enter ECU to update with argument -e (either 118 or 148)" >&2
-  exit 
+  exit 1
 fi
 
-
-
-# add 118 srec directory
-dir_118="/a/gitclone/Gridania_RealTime118/GCC/release/gridania-realtime.srec"
-dir_148="/a/gitclone/Gridania_Telematic148/GCC/release/gridania-telematic.srec"
-
-# Change RPi device here
-#rpi_version="7"
-
-# add rpi address
-rpi_add="pi@raspberrypi-"
+###Flash sequence starts here
+##update 118 on Gridania
 
 if [ $ecu == "118" ]; then
+
+#copy file to RPI
     scp $dir_118 $rpi_add$rpi_version.local:~/openblt/Host/
+
+#connect to RPI
+#RPI commands in EOF marker
     ssh $rpi_add$rpi_version.local <<EOF
       cd ~/openblt/Host/
       ./BootCommander -s=xcp -t=xcp_can -d=can1 -b=250000 -tid=118 -xid=1 -t1=1000 -t3=2000 -t4=10000 -t5=1000 -t7=2000 gridania-realtime.srec
@@ -52,8 +55,14 @@ EOF
 fi
 
 
+##update 148 on Gridania
 if [ $ecu == "148" ]; then
+
+#copy file to RPI
     scp $dir_118 $rpi_add$rpi_version.local:~/openblt/Host/
+
+#connect to RPI
+#RPI commands in EOF marker
     ssh $rpi_add$rpi_version.local <<EOF
       cd ~/openblt/Host/
       ./BootCommander -s=xcp -t=xcp_can -d=can1 -b=250000 -tid=118 -xid=1 -t1=1000 -t3=2000 -t4=10000 -t5=1000 -t7=2000 gridania-realtime.srec
